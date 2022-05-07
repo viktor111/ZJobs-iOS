67642fd508ca0ff966bb1a44ad453f148073e93a
viktor111 <swifrorlilko@gmail.com>
2022-05-07 09:41:58 +0300

    Add inital layout pages

---
+++ ZJobs/Pages/MyAccountPage.swift
@@ -0,0 +1,20 @@
+//
+//  MyAccountPage.swift
+//  ZJobs
+//
+//  Created by Viktor Draganov on 7.05.22.
+//
+
+import SwiftUI
+
+struct MyAccountPage: View {
+    var body: some View {
+        Text("My Account Page")
+    }
+}
+
+struct MyAccountPage_Previews: PreviewProvider {
+    static var previews: some View {
+        MyAccountPage()
+    }
+}
