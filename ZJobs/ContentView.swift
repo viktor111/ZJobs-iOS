80f6bcb6697b366968ad26a8056b61e3063047a4
viktor111 <swifrorlilko@gmail.com>
2022-05-07 09:21:38 +0300

    Initial Commit

---
+++ ZJobs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZJobs
+//
+//  Created by Viktor Draganov on 7.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZJobs/ZJobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZJobsApp.swift
+//  ZJobs
+//
+//  Created by Viktor Draganov on 7.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ZJobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
